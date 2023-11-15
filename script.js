d2d3fdf8017f939f887734f483de642750c2f812
pequenogafanhoto0001 <150972424+pequenogafanhoto0001@users.noreply.github.com>
2023-11-15 20:14:59 -0300

    arquivos quiz

---
+++ script.js
@@ -0,0 +1,113 @@
+function toggle0() {
+  var vGuias = document.getElementById('conteudo');
+  if (vGuias.style.display == "block") {
+    // se vGuias estiver escondido, exiba-o
+    vGuias.style.display = "none";
+  } else {
+    // se vGuias for exibido, esconda-o
+    vGuias.style.display = "block";
+  }
+}
+
+function toggle1() {
+  var vGuias = document.getElementById('agua');
+  if (vGuias.style.display == "block") {
+    // se vGuias estiver escondido, exiba-o
+    vGuias.style.display = "none";
+  } else {
+    // se vGuias for exibido, esconda-o
+    vGuias.style.display = "block";
+  }
+}
+
+function botao() {
+  window.location.href = "index2.html";
+}
+
+function page() {
+  window.location.href = "index.html";
+}
+
+function antes() {
+  window.location.href = "welcome.html";
+}
+
+function welldone() {
+  window.location.href = "welldone.html";
+}
+
+
+
+function toggle2() {
+  var vGuias = document.getElementById('fogo');
+  if (vGuias.style.display == "block") {
+    // se vGuias estiver escondido, exiba-o
+    vGuias.style.display = "none";
+  } else {
+    // se vGuias for exibido, esconda-o
+    vGuias.style.display = "block";
+  }
+}
+
+function toggle3() {
+  var vGuias = document.getElementById('grama');
+  if (vGuias.style.display == "block") {
+    // se vGuias estiver escondido, exiba-o
+    vGuias.style.display = "none";
+  } else {
+    // se vGuias for exibido, esconda-o
+    vGuias.style.display = "block";
+  }
+}
+
+function toggle4() {
+  var vGuias = document.getElementById('cebola');
+  if (vGuias.style.display == "block") {
+    // se vGuias estiver escondido, exiba-o
+    vGuias.style.display = "none";
+  } else {
+    // se vGuias for exibido, esconda-o
+    vGuias.style.display = "block";
+  }
+}
+
+function toggle5() {
+  var vGuias = document.getElementById('moto');
+  if (vGuias.style.display == "block") {
+    // se vGuias estiver escondido, exiba-o
+    vGuias.style.display = "none";
+  } else {
+    // se vGuias for exibido, esconda-o
+    vGuias.style.display = "block";
+  }
+}
+
+function toggle6() {
+  var vGuias = document.getElementById('teclado');
+  if (vGuias.style.display == "block") {
+    // se vGuias estiver escondido, exiba-o
+    vGuias.style.display = "none";
+  } else {
+    // se vGuias for exibido, esconda-o
+    vGuias.style.display = "block";
+  }
+}
+
+function toggle7() {
+  var vGuias = document.getElementById('lima');
+  if (vGuias.style.display == "block") {
+    // se vGuias estiver escondido, exiba-o
+    vGuias.style.display = "none";
+  } else {
+    // se vGuias for exibido, esconda-o
+    vGuias.style.display = "block";
+  }
+}
+
+
+
+
+
+
+
+
